626c624104a653106d2bce8ded0cd97b53efdd10
mark <marcos.chiu@gmail.com>
2024-06-10 17:28:22 -0300

    feat: final project setup

---
+++ src/main/java/br/ada/caixa/respository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package br.ada.caixa.respository;
+
+import br.ada.caixa.entity.Cliente;
+import br.ada.caixa.entity.TipoCliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
+
+    Optional<Cliente> findByDocumento(String documento);
+
+    List<Cliente> findAllByTipo(TipoCliente tipoCliente);
+
+}
